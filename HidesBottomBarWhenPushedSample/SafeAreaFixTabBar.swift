ac0316f401b378625484843ffda8f6a0fbcef9ac
Shingo Hiraya <shingohiraya@gmail.com>
2018-02-27 17:34:35 +0900

    fixes TabBar problems

---
+++ HidesBottomBarWhenPushedSample/SafeAreaFixTabBar.swift
@@ -0,0 +1,55 @@
+//
+//  SafeAreaFixTabBar.swift
+//  HidesBottomBarWhenPushedSample
+//
+//  Created by hiraya.shingo on 2018/02/27.
+//  Copyright © 2018 hiraya.shingo. All rights reserved.
+//
+
+import UIKit
+
+// reference: https://stackoverflow.com/a/47225653
+
+class SafeAreaFixTabBar: UITabBar {
+    
+    var oldSafeAreaInsets = UIEdgeInsets.zero
+    
+    @available(iOS 11.0, *)
+    override func safeAreaInsetsDidChange() {
+        super.safeAreaInsetsDidChange()
+        
+        if oldSafeAreaInsets != safeAreaInsets {
+            oldSafeAreaInsets = safeAreaInsets
+            
+            invalidateIntrinsicContentSize()
+            superview?.setNeedsLayout()
+            superview?.layoutSubviews()
+        }
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var size = super.sizeThatFits(size)
+        if #available(iOS 11.0, *) {
+            let bottomInset = safeAreaInsets.bottom
+            if bottomInset > 0 && size.height < 50 && (size.height + bottomInset < 90) {
+                size.height += bottomInset
+            }
+        }
+        return size
+    }
+    
+    override var frame: CGRect {
+        get {
+            return super.frame
+        }
+        set {
+            var tmp = newValue
+            if let superview = superview, tmp.maxY !=
+                superview.frame.height {
+                tmp.origin.y = superview.frame.height - tmp.height
+            }
+            
+            super.frame = tmp
+        }
+    }
+}
